9a6960defa5b2dba0cf5f93fbe439180af6e3a72
Macohi <sinconsistencia@gmail.com>
2026-02-09 17:32:43 -0600

    get albumList

--- source/AlbumSelectState.hx
+++ source/AlbumSelectState.hx
@@ -1,7 +1,11 @@
+import macohi.funkin.koya.backend.AssetPaths;
+import macohi.util.StringUtil;
 import macohi.funkin.pre_vslice.MusicBeatState;
 
 class AlbumSelectState extends MusicBeatState
 {
+	public var albumList:Array<String> = StringUtil.splitTextAssetByNewlines(AssetPaths.txt('data/albumList'));
+
 	override public function create()
 	{
 		super.create();
